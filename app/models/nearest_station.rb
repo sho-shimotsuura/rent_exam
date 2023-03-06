a9ba1c31860aa049031e3480704948216d234352
下津浦勝 <shimotsuurasho@shitatsuuramasarunoMacBook-Air.local>
2023-03-06 16:51:14 +0900

    課題再提出

--- app/models/nearest_station.rb
+++ app/models/nearest_station.rb
@@ -1,3 +1,6 @@
 class NearestStation < ApplicationRecord
   belongs_to :property
+  validates :line_name, presence: true
+  validates :station_name, presence: true
+  validates :time, presence: true
 end
